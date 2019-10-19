fcdc1a075913e2dc317db672dc1235b2d74ebd3c
prashrey <prashrey04@gmail.com>
2019-10-20 03:04:36 +0530

    Album cards initial design

--- src/common/AlbumCard.js
+++ src/common/AlbumCard.js
@@ -1,12 +1,18 @@
 import React from 'react';
+// import { url } from 'inspector';
 
 export default function AlbumCard(props) {
     return (
         <div className="album-grid">
             {props.results.map((album, i) => {
+                let imageUri = album.image[3]["#text"];
                 return (
-                    <div key={i} className="album-card">
-                        {album.name}
+                    <div key={i} className="album-card" style={{ backgroundImage: "url(" + imageUri + ")" }}>
+                        <div className="album-gradient"></div>
+                        <div className="album-info">
+                            <h5 className="h5 album-artist">{album.artist}</h5>
+                            <div className="album-name">{album.name}</div>
+                        </div>
                     </div>
                 )
             })}
--- src/presentation/SearchWidgetV2.js
+++ src/presentation/SearchWidgetV2.js
@@ -16,15 +16,19 @@ export default class SearchWidget extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            inputSelected: ''
+            inputSelected: false,
+            inputSelectedValue: ''
         }
     }
     updateSelectedValue = (selectedValue) => {
-        this.setState({ inputSelected: selectedValue })
+        this.setState({ inputSelectedValue: selectedValue })
+    }
+    hideWidget = () => {
+        this.setState({ inputSelected: true })
     }
     render() {
         return (
-            <section className="row search-widget">
+            <section className={"row search-widget " + (this.state.inputSelected ? "d-none" : "")}>
                 <div className="col-md-7 category-wrapper">
                     <div className="category">
                         <i className="fas fa-music"></i>
@@ -34,7 +38,7 @@ export default class SearchWidget extends React.Component {
                 </div>
                 <div className="col-md-5 input-wrapper">
                     <SelectDropdown suggestionList={userOptions} returnCallback={this.updateSelectedValue} />
-                    <Button class="button button-primary" clickEvent={null} disabled={false}>Find me an Album </Button>
+                    <Button class="button button-primary" clickEvent={this.hideWidget} disabled={false}>Find me an Album </Button>
                 </div>
             </section >
         );
